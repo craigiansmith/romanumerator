bcdaf007c4139b16867385344f730fe0346ba9fa
Craig Smith <hello@craigiansmith.com.au>
2021-03-02 12:02:40 +1100

    Count numerals up to D for 500

--- lib/romanumerator.rb
+++ lib/romanumerator.rb
@@ -1,12 +1,16 @@
 class Adder
   def count(input)
     # Create a queue from the input string
-    numerals = input.scan /I|V|U|L|C|D|M/
+    numerals = input.scan /I|V|U|X|L|C|D|M/
     # Sum the values of each element in the queue
     sum = 0
     numerals.each do |number|
       sum += 1 if number == 'I'
       sum += 5 if number == 'V' or number == 'U'
+      sum += 10 if number == 'X'
+      sum += 50 if number == 'L'
+      sum += 100 if number == 'C'
+      sum += 500 if number == 'D'
     end
     sum
   end
--- spec/adder_spec.rb
+++ spec/adder_spec.rb
@@ -20,4 +20,28 @@ RSpec.describe Adder, "#count" do
      expect(adder.count('U')).to eq(5)
    end
  end
+ context "with X" do
+   it "counts the value 10" do
+     adder = Adder.new
+     expect(adder.count('X')).to eq(10)
+   end
+ end
+ context "with L" do
+   it "counts the value 50" do
+     adder = Adder.new
+     expect(adder.count('L')).to eq(50)
+   end
+ end
+ context "with C" do
+   it "counts the value 100" do
+     adder = Adder.new
+     expect(adder.count('C')).to eq(100)
+   end
+ end
+ context "with D" do
+   it "counts the value 500" do
+     adder = Adder.new
+     expect(adder.count('D')).to eq(500)
+   end
+ end
 end 
