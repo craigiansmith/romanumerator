cec63cd7d2ad3a163d5e84926d996df25c57cf04
Craig Smith <hello@craigiansmith.com.au>
2021-03-02 11:57:49 +1100

    Make the adder count

--- lib/romanumerator.rb
+++ lib/romanumerator.rb
@@ -1,5 +1,13 @@
 class Adder
-  def count(numerals)
-    1
+  def count(input)
+    # Create a queue from the input string
+    numerals = input.scan /I|V|U|L|C|D|M/
+    # Sum the values of each element in the queue
+    sum = 0
+    numerals.each do |number|
+      sum += 1 if number == 'I'
+      sum += 5 if number == 'V' or number == 'U'
+    end
+    sum
   end
 end
--- spec/adder_spec.rb
+++ spec/adder_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe Adder, "#count" do
     expect(adder.count('I')).to eq(1)
   end
  end
+ context "with III" do
+   it "counts the value 3" do
+     adder = Adder.new
+     expect(adder.count('III')).to eq(3)
+   end
+ end
+ context "with V or U" do
+   it "counts the value 5" do
+     adder = Adder.new
+     expect(adder.count('V')).to eq(5)
+     expect(adder.count('U')).to eq(5)
+   end
+ end
 end 
